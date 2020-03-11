0538b090a7a25cf9f343d33655f9b6c8f0e163c6
stefan Haslinger <mail@stefan-haslinger.at>
2020-03-11 17:04:04 +0100

    getting the wikitext

--- example/wikipedia_reader_example.dart
+++ example/wikipedia_reader_example.dart
@@ -1,6 +1,7 @@
 import 'package:wikipedia_reader/wikipedia_reader.dart';
 
-void main() {
-  var queryString = 'St.Pölten';
-  Article(queryString: queryString);  
+void main() async {
+  var queryString = 'St. Pölten';
+  var text = await Article().download(title: queryString);  
+  print(text);
 }
--- lib/src/article.dart
+++ lib/src/article.dart
@@ -1,6 +1,16 @@
-class Article {
-  String queryString;
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
-  Article({this.queryString}) {
+class Article {
+
+  Future<String> download({String title}) async {
+    var baseUri = 'https://de.wikipedia.org/w/api.php?';
+    var params = 'action=query&prop=revisions&rvprop=content&format=json&&titles=';
+    var url = baseUri + params + title;
+    var response = await http.get(url);
+    var wikitext = json.decode(response.body)['query']['pages'].values.first['revisions'].first['*'];
+    print(wikitext);
+
+    return 'done';
   }
 }
